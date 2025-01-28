275303dd37f74ada44107e64034689ed4a43687e
nandini-chakali <chakalinandini31@gmail.com>
2025-01-28 09:46:47 +0530

    Add files via upload

---
+++ Asciivalues.java
@@ -0,0 +1,11 @@
+import java.util .Scanner;
+public class Asciivalues {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("enter the string");
+        char ch=scanner.next().charAt(0);
+        System.out.println("ASCII values are"+(int)(ch));
+        
+    }
+    
+}
